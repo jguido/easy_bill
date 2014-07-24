90de2d5de8b394e008bab25a5a445db0a7e6a437
= <webmaster@unrtech.fr>
2014-07-24 21:45:47 +0200

    Fix fucking deploy file 1.3

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,21 +1,12 @@
-set :application, "Easy Bill"
-set :domain,      "bill.unrtech.fr"
-set :deploy_to,   "/home/www-data/src/easyBill"
-set :app_path,    "app"
+set   :application,   "Easy Bill"
+set   :deploy_to,     "/home/www-data/src/easyBill"
+set   :domain,        "195.154.9.83"
 
-set :repository,  "file:///home/unrtech/repo/easyBill.git"
-set :scm,         :git
-set :deploy_via,  :copy
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+set   :scm,           :git
+set   :repository,    "ssh-git195.154.9.83:/home/unrtech/repo/easyBill.git"
 
-set :model_manager, "doctrine"
-# Or: `propel`
-
-role :web,        domain                         # Your HTTP server, Apache/etc
-role :app,        domain, :primary => true       # This may be the same as your `Web` server
+role  :web,           domain
+role  :app,           domain, :primary => true
 
 set   :use_sudo,      false
-set  :keep_releases,  3
-
-# Be more verbose by uncommenting the following line
-# logger.level = Logger::MAX_LEVEL
+set   :keep_releases, 3
