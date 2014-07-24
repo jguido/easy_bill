7f7ddc0f58f3eea71505f7b701488f23ef6b79a6
= <webmaster@unrtech.fr>
2014-07-24 21:17:50 +0200

    Add capifony

---
+++ app/config/deploy.rb
@@ -0,0 +1,19 @@
+set :application, "Easy Bill"
+set :domain,      "bill.unrtech.fr"
+set :deploy_to,   "/home/www-data/src/easyBill"
+set :app_path,    "app"
+
+set :repository,  "/home/unrtech/repo/easyBill.git"
+set :scm,         :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+
+set :model_manager, "doctrine"
+# Or: `propel`
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
+
+set  :keep_releases,  3
+
+# Be more verbose by uncommenting the following line
+# logger.level = Logger::MAX_LEVEL
